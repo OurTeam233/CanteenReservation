962d6034684c90d3246f1d3fd4f6e7be5220c968
tang <940012301@qq.com>
2021-11-25 10:13:43 +0800

    个人信息

--- wx-app/pages/community/index.js
+++ wx-app/pages/community/index.js
@@ -5,62 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    activePage: 1,
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 标签切换事件
+  changePage (event) {
+    wx.showToast({
+      title: `切换到标签 ${event.detail.name}`,
+      icon: 'none',
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onChange (e) {
+    this.setData({
+      value: e.detail,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onSearch () {
+    console.log(this.data.value)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onClick () {
+    console.log(this.data.value)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
