aea4c71bf8ee32f2af7386db0e2ae8f3c65a231a
LAPTOP-E30CFTJJ\SUN <3027615259@qq.com>
2022-05-26 14:08:11 +0800

    新增聊天

---
+++ wx-app/pages/chat/index.js
@@ -0,0 +1,85 @@
+// pages/chat/index.js
+Page({
+
+  data: {
+    person:[
+      {
+        nike:'伟豪',
+        picture:"../../image/headPhone/head1.webp",
+        unread:2,
+        time:'2020.1.9',
+        word:'你在干嘛'
+      },
+      {
+        nike:'小乔',
+        picture:"../../image/headPhone/head2.webp",
+        unread:1,
+        time:'周三',
+        word:'你那个锅多少钱20块钱可以买得到吗？可以便宜点吗'
+      },
+      {
+        nike:'三松许',
+        picture:"../../image/headPhone/head3.webp",
+        unread:0,
+        time:'周三',
+        word:'你这要零食吗'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
