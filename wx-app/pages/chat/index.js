e9476ebd91bdf5c8dfec2ddc324c2d41bbe86d8c
LAPTOP-E30CFTJJ\SUN <3027615259@qq.com>
2022-05-26 16:53:32 +0800

    聊天页面

--- wx-app/pages/chat/index.js
+++ wx-app/pages/chat/index.js
@@ -5,12 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
+    person:[
+      {
+        nike:"灯火",
+        img:"../../image/headPhone/head1.webp",
+        time:"2022.05.06",
+        unread:13,
+        words:"你的过怎么卖的"
+      },
+      {
+        nike:"蓝色大海的泥土",
+        img:"../../image/headPhone/head3.webp",
+        time:"周一",
+        unread:8,
+        words:"剪一段烛光,独坐一隅,梦一段红尘,思忆一番。其实,回不去的日子犹如倒映在天蓝色湖面的孤单的影子。"
+      },
+      {
+        nike:"珊瑚",
+        img:"../../image/headPhone/head2.webp",
+        time:"周三",
+        unread:1,
+        words:"你的过怎么卖的"
+      }
+    ]
 
   },
 
   /**
    * 生命周期函数--监听页面加载
-   */
+   */ 
   onLoad: function (options) {
 
   },
